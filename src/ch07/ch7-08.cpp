8a3ede844cff9a08e9e54b7834259ff9d87cda67
49cel <pranav@Pranavs-MacBook-Air-3.local>
2026-08-11 12:05:01 +0530

    more addition to bad usage of global variables

--- src/ch07/ch7-08.cpp
+++ src/ch07/ch7-08.cpp
@@ -1,3 +1,9 @@
 /* a c++ joke: what's the best naming prefix for a global variable?
  * answer: //
  * we genuinely got the whole squad laughing */
+
+int g_x{ initX() };   // runs first
+int g_y{ initY() };   // runs second
+
+int initX() { return g_y; }   // tries to use g_y, but g_y hasn't been set yet!
+int initY() { return 5; }
