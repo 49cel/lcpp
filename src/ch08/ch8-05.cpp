a68de865ad865ab462c37a06760caefb08c2e7f0
pranavponnada <pranavponnada07@gmail.com>
2026-08-26 03:34:57 +1000

    src: 8.5 added

---
+++ src/ch08/ch8-05.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void printDigitName(int x)
+{
+    switch (x) // x evaluates to 3
+    {
+        case 1:
+            std::cout << "One";
+            break;
+        case 2:
+            std::cout << "Two";
+            break;
+        case 3:
+            std::cout << "Three"; // execution starts here
+            break; // jump to the end of the switch block
+        default:
+            std::cout << "Unknown";
+            break;
+    }
+
+    // execution continues here
+    std::cout << " Ah-Ah-Ah!";
+}
+
+int main()
+{
+    printDigitName(3);
+    std::cout << '\n';
+
+    return 0;
+}
