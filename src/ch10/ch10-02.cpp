812f63fad5e108c1a3d47264b228a44ad44873c7
pranavponnada <pranavponnada07@gmail.com>
2026-08-30 16:29:48 +1000

    src: 10.2 added

---
+++ src/ch10/ch10-02.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void printDouble(double d)
+{
+    std::cout << d << '\n';
+}
+
+
+void printInt(int x)
+{
+    std::cout << x << '\n';
+}
+
+int main()
+{
+    printDouble(5.0); // no conversion necessary
+    printDouble(4.0f); // numeric promotion of float to double
+
+    printInt(2);
+
+    short s{ 3 }; // there is no short literal suffix, so we'll use a variable for this one
+    printInt(s); // numeric promotion of short to int
+
+    printInt('a'); // numeric promotion of char to int
+
+    return 0;
+}
