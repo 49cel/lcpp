bcacde38ba9e460a30ecc8c7f9c5c5f2628b31eb
pranavponnada <pranavponnada07@gmail.com>
2026-08-30 16:10:13 +1000

    src: 10.1 added

---
+++ src/ch10/ch10-01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    int n { 3 };                        // here's int value 3
+    float f {};                         // here's our float variable
+    std::memcpy(&f, &n, sizeof(float)); // copy the bits from n into f
+    std::cout << f << '\n';             // print f (containing the bits from n)
+
+    return 0;
+}
+
+// this is to see what happens if you try to use the same state of bits for a different data type without type conversion
